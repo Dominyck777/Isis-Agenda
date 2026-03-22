0d402bc47c1107238e58d8dfb6832abf405aa170
Kallew Felip <85791728+yEuzy@users.noreply.github.com>
2026-03-22 13:56:51 -0300

    webapp

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+// Simple Service Worker for PWA
+const CACHE_NAME = 'isis-agenda-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/pwa-192.png',
+  '/pwa-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
